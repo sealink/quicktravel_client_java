9fb78a0352a00b0f0b2ed30be2b7ff7cee04f1d4
John Mortlock <john.mortlock@gmail.com>
2018-06-26 14:13:11 +0930

    Add spec

---
+++ src/test/java/au/com/sealink/quicktravel/client/models/EncryptedDataTest.java
@@ -0,0 +1,21 @@
+package au.com.sealink.quicktravel.client.models;
+
+import static junit.framework.TestCase.assertEquals;
+
+import com.google.gson.Gson;
+import org.junit.Test;
+import au.com.sealink.quicktravel.client.helpers.FixtureHelper;
+
+public class EncryptedDataTest {
+
+    private void assertData(EncryptedData actual) {
+        assertEquals("Error Message", actual.getEncryptedData());
+    }
+
+    @Test
+    public void fromJson() {
+        String json = FixtureHelper.fromFile("fixtures/encrypted_data.json");
+        EncryptedData actual = new Gson().fromJson(json, EncryptedData.class);
+        assertData(actual);
+    }
+}
